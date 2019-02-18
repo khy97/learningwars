caac8fa43c0f95de23ac98704a330e7da2d0fc0e
Le Van Tuan Long <tuanlong.levan@gmail.com>
2019-02-18 23:49:26 +0800

    Added Long's description

--- src/templates/Authors.js
+++ src/templates/Authors.js
@@ -5,6 +5,12 @@ var authors = {
         authorDesc: "A passionate learner and a writer. Penultimate Student in Singapore Management University, pursuing Bs. in Information Systems with Analytics as 2nd major. Aspires to be a Professional Software Developer in the future.",
         linkedIn: "https://www.linkedin.com/in/huiyeonkim/"
     },
+    "Le_Van_Tuan_Long" : {
+        authorName: "Le Van Tuan Long",
+        authorTag: "Software Developer | Data Scientist | Product Builder",
+        authorDesc: "Programming is an extremely creative profession. It is logic-based creativity. Always on the lookout to learn and acquire new knowledge in different areas. Hope to use technology skill to build great products that transform lives",
+        linkedin: "https://www.linkedin.com/in/levanify/"
+    }
 }
 
 module.exports = authors;
