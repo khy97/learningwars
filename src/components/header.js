2b8b52374937923f57eeb378fb6ef75bd9f6bf73
Kim Huiyeon <gnldus59112@gmail.com>
2019-12-25 01:29:15 +0530

    Fix Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { navigate } from 'gatsby'
 
 const Header = ({number}) => {
-    let location = "";
-    if(typeof window !== `undefined`) {
-        location = window.location.href;
-    }
-    const check = location[location.length - 1] === 'https://learnwars.com/' || location[location.length - 1] === 'https://learnwars.com' || location[location.length - 1] === 'https://www.learnwars.com/' || location[location.length - 1] === 'https://www.learnwars.com'
-    console.log(location)
+    const [check, setCheck] = useState(true);
+
+    useEffect(() => {
+        let location = "";
+        if(typeof window !== `undefined`) {
+            location = window.location.href;
+        }
+        const checkk = location[location.length - 1] === 'https://learnwars.com/' || location[location.length - 1] === 'https://learnwars.com' || location[location.length - 1] === 'https://www.learnwars.com/' || location[location.length - 1] === 'https://www.learnwars.com'
+        setCheck(checkk);
+    },[])
     return (
         <div style={{width:`95%`, margin:`auto`, marginTop:25}}>
             <h2 style={!check ? {fontFamily:`Raleway`, fontSize:20, cursor:`pointer`} : {fontFamily:`Raleway`, fontSize:20}} onClick={() => navigate('/')}>{!check ? '< Back to all posts': `(${number}) Recent Blog Posts`}</h2>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import {Link} from 'gatsby';
-import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const NotFoundPage = () => (
-  <Layout>
+  <div>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p style={{fontFamily: `Crimson Text, serif`}}>You just hit a route that doesn&#39;t exist... the sadness.</p>
     <Link to='/' style={{textDecoration:`none`, color: `white`, width: 200, padding:`20px 0`, background:`#d9534f`, display:`block`, textAlign:`center`, borderRadius:4, boxShadow:`0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)`, fontFamily:`Crimson Text, serif`}}>Go back Home</Link>
-  </Layout>
+  </div>
 )
 
 export default NotFoundPage
